10fa84438d2b32a25d0d871d436e24501a7e7f38
Wimurk <wimurk@users.noreply.github.com>
2019-09-30 16:10:59 +0200

    Added new method

--- src/Traits/Cacher.php
+++ src/Traits/Cacher.php
@@ -8,14 +8,14 @@ trait Cacher
 {
     /**
      * 
-     * @param type $query
+     * @param \Illuminate\Database\Query\Builder $builder
      * @param int $ttl | default 1 week
      * @return \Illuminate\Contracts\Cache\Repository
      */
     public function scopeRemember(Builder $builder, int $ttl = 86400*7)
     {
-        $query = str_replace(array('?'), array('\'%s\''), $builder->toSql());
-        $query = vsprintf($query, $builder->getBindings());
+        $queryString = str_replace(array('?'), array('\'%s\''), $builder->toSql());
+        $query = vsprintf($queryString, $builder->getBindings());
         return Cache::remember(md5($query), $ttl, function() use($builder){
             return $builder->get();
         });
@@ -23,13 +23,28 @@ trait Cacher
     
     /**
      * 
-     * @param type $query
+     * @param \Illuminate\Database\Query\Builder $builder
+     * @param int $ttl | default 1 week
+     * @return \Illuminate\Contracts\Cache\Repository
+     */
+    public function scopeFindAndRemember(Builder $builder, $find, int $ttl = 86400*7)
+    {
+        $queryString = str_replace(array('?'), array('\'%s\''), $builder->toSql());
+        $query = vsprintf($queryString, $builder->getBindings());
+        return Cache::remember(md5($query), $ttl, function() use($builder, $find){
+            return $builder->find($find);
+        });
+    }
+    
+    /**
+     * 
+     * @param \Illuminate\Database\Query\Builder $builder
      * @return \Illuminate\Contracts\Cache\Repository
      */
     public function scopeRememberForever(Builder $builder)
     {
-        $query = str_replace(array('?'), array('\'%s\''), $builder->toSql());
-        $query = vsprintf($query, $builder->getBindings());
+        $queryString = str_replace(array('?'), array('\'%s\''), $builder->toSql());
+        $query = vsprintf($queryString, $builder->getBindings());
         return Cache::rememberForever(md5($query), function() use($builder){
             return $builder->get();
         });
